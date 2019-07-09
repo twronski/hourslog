81a5365ff7f955d80567860fdde07adfbb699672
Thiago Wronski <wronski79@gmail.com>
2019-07-08 23:21:30 -0300

    Add Project Scafold

---
+++ db/migrate/20190709021419_create_projects.rb
@@ -0,0 +1,11 @@
+class CreateProjects < ActiveRecord::Migration[5.2]
+  def change
+    create_table :projects do |t|
+      t.string :wbs
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
