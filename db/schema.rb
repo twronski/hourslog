b26ccf9033550220701da8db7c715d735e53d66d
Thiago Wronski <wronski79@gmail.com>
2019-07-09 14:35:36 -0300

    Remove hour log and report models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_023642) do
+ActiveRecord::Schema.define(version: 2019_07_09_021419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,17 +21,6 @@ ActiveRecord::Schema.define(version: 2019_07_09_023642) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "reports", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "project_id"
-    t.integer "status"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["project_id"], name: "index_reports_on_project_id"
-    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,6 +35,4 @@ ActiveRecord::Schema.define(version: 2019_07_09_023642) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "reports", "projects"
-  add_foreign_key "reports", "users"
 end
