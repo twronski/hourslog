bfdb3f5b43064e367e88f9d902c31c9f8c6236e8
Thiago Wronski <thiagowronski@187-9-148-162.customer.tdatabrasil.net.br>
2019-07-25 15:28:57 -0300

    Create enum for hour record status and list roles on the Readmefile

--- app/models/hours_record.rb
+++ app/models/hours_record.rb
@@ -1,4 +1,5 @@
 class HoursRecord < ApplicationRecord
+  enum status: %i(new under_analysis under_revision aut_approved approved aut_rejected rejected)
   belongs_to :activity
   belongs_to :project
   belongs_to :user
