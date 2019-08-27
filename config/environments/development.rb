1a6c3dbeda588b86c87616d3f0ef57162d351012
Thiago Wronski <wronski79@gmail.com>
2019-08-26 23:45:40 -0300

    Fix mailcatcher issue => port 1025

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # Configs for setup mailcatcher and devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => 'mailcatcher', :port => 25 }
+  config.action_mailer.smtp_settings = { :address => 'mailcatcher', :port => 1025 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
