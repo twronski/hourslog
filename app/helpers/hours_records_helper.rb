030c072f51313d034ccb3668a7677064f215c936
Thiago Wronski <wronski79@gmail.com>
2019-08-29 21:33:21 -0300

    Refactor Helper

--- app/helpers/hours_records_helper.rb
+++ app/helpers/hours_records_helper.rb
@@ -1,11 +1,9 @@
 module HoursRecordsHelper
-    def hour_status(status)
-        statuses_hash = {"rep_under_analysis" => {"message" =>'Em Análise', "badge" => "badge badge-primary"},
-            "rep_under_revision" =>  {"message" =>'Em revisão', "badge" => "badge badge-warning"}, 
-            "rep_aut_approved" => {"message" => 'Aprovado Aut', "badge" => "badge badge-success"}, 
-            "rep_approved" => {"message" => 'Aprovado', "badge" => "badge badge-success"}, 
-            "rep_aut_rejected" => {"message" => 'Reprovado Aut', "badge" => "badge badge-danger"}, 
-            "rep_rejected"  => {"message" => 'Reprovado', "badge" => "badge badge-danger"}}
-        statuses_hash[status]
-    end
+    STATUSES_HASH = {"rep_under_analysis" => {"message" =>'Em Análise', "badge" => "badge badge-primary"},
+        "rep_under_revision" =>  {"message" =>'Em revisão', "badge" => "badge badge-warning"}, 
+        "rep_aut_approved" => {"message" => 'Aprovado Aut', "badge" => "badge badge-success"}, 
+        "rep_approved" => {"message" => 'Aprovado', "badge" => "badge badge-success"}, 
+        "rep_aut_rejected" => {"message" => 'Reprovado Aut', "badge" => "badge badge-danger"}, 
+        "rep_rejected"  => {"message" => 'Reprovado', "badge" => "badge badge-danger"}}
+
 end
