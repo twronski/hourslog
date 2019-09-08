8a3228d2112a65b69c880436857accc0482059ea
Thiago Wronski <thiagowronski@localhost.localdomain>
2019-09-08 00:53:03 -0300

    Rateyo some view changes

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -9,11 +9,22 @@ $(function () {
  
     rating    : 1.6,
     numStars  : 10,
+    maxValue  : 10,
     spacing   : "5px",
     multiColor: {
  
       "startColor": "#FF0000", //RED
       "endColor"  : "#00FF00"  //GREEN
+    },
+
+    onChange: function (rating, rateYoInstance) {
+ 
+      $(this).next().text(rating);
+    },
+
+    onSet: function (rating, rateYoInstance) {
+ 
+      alert("Rating is set to: " + rating);
     }
   });
  
