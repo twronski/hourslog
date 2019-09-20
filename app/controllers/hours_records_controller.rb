fd5126d2e5763499732ed0921b2822671f4a45ee
Thiago Wronski <wronski79@gmail.com>
2019-09-20 11:45:55 -0300

    Fix comments including workflow

--- app/controllers/hours_records_controller.rb
+++ app/controllers/hours_records_controller.rb
@@ -1,10 +1,10 @@
 class HoursRecordsController < ApplicationController
-  before_action :set_hours_record, only: [:show, :edit, :update, :destroy, :show_workflow]
+  before_action :set_hours_record, only: [:show, :edit, :update, :destroy, :show_workflow, :set_workflow]
 
   # GET /hours_records
   # GET /hours_records.json
   def index
-    @hours_records = HoursRecord.all.includes(:activity, :bay, :improductive_reason, :main_skill, :project, :voltage_level, :profile)
+    @hours_records = HoursRecord.all.includes(:activity, :bay, :improductive_reason, :main_skill, :main_equipment, :project, :voltage_level, :profile)
   end
 
   # GET /hours_records/1
@@ -15,8 +15,8 @@ class HoursRecordsController < ApplicationController
   # GET /hours_records/new
   def new
     @hours_record = HoursRecord.new
-    logger.info current_user.profile.id
-    @hours_record.comments.build(profile_id: current_user.profile.id)
+    # logger.info current_user.profile.id
+    # @hours_record.comments.build(profile_id: current_user.profile.id)
   end
 
   # GET /hours_records/1/edit
@@ -46,7 +46,8 @@ class HoursRecordsController < ApplicationController
   # PATCH/PUT /hours_records/1.json
   def update
     respond_to do |format|
-       @hours_record.set_under_analisys
+      self.set_workflow
+       
       if @hours_record.update(hours_record_params)
         format.html { redirect_to @hours_record, notice: 'Hours record was successfully updated.' }
         format.json { render :show, status: :ok, location: @hours_record }
@@ -69,24 +70,20 @@ class HoursRecordsController < ApplicationController
 
   def set_workflow
 
-    @hours_record = HoursRecord.find(params[:hours_record][:id])
-    
     if params[:commit] ==  'Reprovar Horas'
       @hours_record.set_rejected
     elsif params[:commit] == 'Aprovar Horas'
       @hours_record.set_approved
     elsif params[:commit] == 'Devolver para Revisão'
       @hours_record.set_under_revision
+    else
+      @hours_record.set_under_analisys
     end
-
-    # @hours_record = HoursRecord.first
-    # @hours_record.update(status: "rep_under_revision")
-    
-    redirect_to @hours_record
 
   end
 
   def show_workflow
+    @hours_record.comments.build(profile_id: current_user.profile.id)
     
   end
   
@@ -95,13 +92,16 @@ class HoursRecordsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_hours_record
+      #  @hours_record = HoursRecord.where(id: params[:id]).includes(:activity, :bay, :improductive_reason, :main_skill, :main_equipment, :project, :voltage_level, :profile).first
       @hours_record = HoursRecord.find(params[:id])
-    end
+      #  @hours_record = HoursRecord.includes(:activity, :bay, :improductive_reason, :main_skill, :main_equipment, :project, :voltage_level, :profile).find(params[:id])
+      # @hours_record = HoursRecord.all.includes(:activity, :bay, :improductive_reason, :main_skill, :main_equipment, :project, :voltage_level, :profile).find(params[:id])
 
+    end
     # Never trust parameters from the scary internet, only allow the white list through.
     def hours_record_params
       params.require(:hours_record).permit(:day, :man_hour, :activity_id, :project_id, :profile_id, :bay_id, :improductive_reason_id,
-                                           :voltage_level_id, :main_skill_id,:record_doc, :description, :improductive ,
+                                           :voltage_level_id, :main_skill_id,:main_equipment_id,:record_doc, :description, :improductive ,
                                           comments_attributes: [:id, :body, :profile_id])
     end
     
